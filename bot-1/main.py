031b7a9d139df9629b0869866f3a0eae4a7a610f
shesh <79000021+carlo-vbs@users.noreply.github.com>
2021-09-23 18:41:38 +0800

    last updated ig

--- bot-1/main.py
+++ bot-1/main.py
@@ -3,6 +3,24 @@ import discord
 import requests
 import json
 import dotenv
+from replit import db
+import random
+
+sad_words = ['sad',
+  'depressed',
+  'unhappy',
+  'angry',
+  'depressing',
+  'miserable'
+]
+
+starter_encourage=[
+  'Cheer up',
+  'Cheer up! bitch',
+  'bitch',
+  'emo',
+  'hang in ther'
+]
 
 client = discord.Client()
 dotenv.load_dotenv()
@@ -12,6 +30,18 @@ def get_quote():
   json_data=json.loads(response.text)
   quote=json_data[0]['q'] + ' \n-'+json_data[0]['a']+'-'
   return quote
+
+def update_encouragements(message):
+  if 'encouragements' in db.keys():
+    db['encouragements']=db['encouragements'].append(message)
+  else:
+    db['encouragements']=[message]
+
+def delete_encouragement(ind):
+  enc=db['encouragements']
+  if len(enc)>ind:
+    del enc[ind]
+    db['encouragements']=enc
 
 @client.event
 async def on_ready():
@@ -30,5 +60,28 @@ async def on_message(message):
     await message.channel.send('duh, you shut up! im busy, tsk')
   if message.content.startswith('$inspire'):
     await message.channel.send(get_quote())
+  msg=message.content
+  
+  options=['u can do it!'] + starter_encourage
+
+  if 'encouragements' in db.keys():
+    options+=db['encouragements']
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(options))
+
+  if message.content.startswith('$new'):
+    enc=msg.split('$new ',1)[1]
+    update_encouragements(enc)
+    await message.channel.send('new encouraging message... added  \n thank u \n bitch')
+
+  if message.content.startswith('$del'):
+    enc=[]
+    if 'encouragements' in db.keys():
+      ind = msg.split('$del',1)[1]
+      delete_encouragement(ind)
+      enc = db['encouragements']
+    await message.channel.send('new encouraging message... added \n thank u \n bitch')
+  
 
 client.run(os.getenv('myBotToken'))
